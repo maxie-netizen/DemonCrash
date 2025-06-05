3b838bf313ff291626e60aa002edd5b1a9e1a89f
maxie-netizen <maxwellirungu64@gmail.com>
2025-06-05 11:59:51 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,53 @@
+/*
+  [ NOMER GANTI PAKE NO BOT KAMU ]
+*/
+global.owner = ['GANTI NOMER KAMU']
+global.ownername = "NAMA KALIAN"
+global.namabot = "NAMA SC LU" 
+global.connect = true // False = QrCode
+global.antilink = false
+global.autotyping = true
+global.welcome = false
+global.autoreadsw = false
+global.autorecord = true
+
+/*
+  [ PAYMENT BEBAS ]
+*/
+global.dana = "NOMER KALIAN"
+global.gopay = "NOMER KALIAN"
+global.ovo = "NOMER KALIAN"
+global.qris = "LINK QR KALIAN "
+
+/*
+  [ GAUSAH DI UBAH ]
+*/
+global.mess = {
+"ketua": "Akses Di Tolak!",
+"prem": "Akses Di Tolak!",
+"premium": "Akses Di Tolak!",
+"japost": "Akses Di Tolak!",
+"rekber": "Akses Di Tolak!",
+"owner": "Akses Di Tolak!"
+}
+
+/*
+  [ ISI PUNYA KALIAN ]
+*/
+global.reply = "https://img1.pixhost.to/images/5812/599772489_imgtmp.jpg"
+global.tele = "ISI SENDIRI"
+global.tele2 = "ISI SENDIRI"
+global.waMe = "wa.me/ISI SENDIRI"
+global.tutorialBot = "ISI SENDIRI"
+global.versionofscript = "ISI SENDIRI"
+global.url = "ISI SENDIRI"
+global.urlbanner = "ISI SENDIRI"
+global.url2 = "ISI SENDIRI"
+global.packname = "ISI SENDIRI"
+global.author = "ISI SENDIRI"
+global.group = "ISI SENDIRI"
+global.idCH = "120363398308001247@newsletter"
+global.xchannel = {
+	jid: '120363398308001247@newsletter'
+	}
+	
